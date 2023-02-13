a8154b7c563dc9faeae99223b03a4280fb818a71
Gerrit Kieffer <kieffer.gerrit@gmail.com>
2023-02-13 10:31:40 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,62 @@
+import argparse
+import logging
+import os
+import sys
+import time
+import uuid
+from datetime import datetime, timezone
+from json import load
+from logging.config import dictConfig
+from os.path import join, dirname, realpath
+
+from servicebus import get_receiver, get_sender
+
+logger = logging.getLogger(__name__)
+
+
+def setup_logging():
+    with open(join(dirname(realpath(__file__)), 'logging.json'), 'rt') as f:
+        config = load(f)
+        dictConfig(config)
+        logging.Formatter.converter = time.gmtime
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Simple producer / consumer for debugging the azure servicebus")
+    parser.add_argument("mode", choices=['producer', 'consumer'])
+    args = parser.parse_args(sys.argv[1:])
+    logger.info(f"Arguments: {args}")
+    return args
+
+
+def produce_messages(connection_string, queue_name):
+    with get_sender(connection_string, queue_name) as send:
+        while True:
+            test = {'id': str(uuid.uuid4()), 'ts': datetime.now(tz=timezone.utc).isoformat().replace("+00:00", "Z")}
+            send(test)
+            time.sleep(1)
+
+
+def consume_messages(connection_string, queue_name):
+    with get_receiver(connection_string, queue_name) as receive:
+        while True:
+            message = receive()
+
+
+def main():
+    setup_logging()
+    args = parse_args()
+
+    connection_string = os.environ.get('CONNECTION_STRING', '')
+    queue_name = os.environ.get('QUEUE_NAME', '')
+
+    if args.mode == 'producer':
+        produce_messages(connection_string, queue_name)
+    elif args.mode == 'consumer':
+        consume_messages(connection_string, queue_name)
+    else:
+        logger.error(f'Invalid mode: {args.mode}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/servicebus.py
@@ -0,0 +1,36 @@
+import json
+import logging
+import timeit
+from contextlib import contextmanager
+
+from azure.servicebus import AutoLockRenewer, ServiceBusClient, ServiceBusMessage
+
+logger = logging.getLogger(__name__)
+
+@contextmanager
+def get_receiver(connection_string, queue_name):
+    with ServiceBusClient.from_connection_string(connection_string) as client, AutoLockRenewer(
+            max_lock_renewal_duration=5 * 60, max_workers=1) as auto_lock_renewer, client.get_queue_receiver(
+        queue_name=queue_name, auto_lock_renewer=auto_lock_renewer) as receiver:
+        def receive_message():
+            messages = receiver.receive_messages()
+            if messages:
+                message = json.loads(str(messages[0]))
+                logger.info(f"Received message {message}")
+                receiver.complete_message(messages[0])
+                return message
+
+        yield receive_message
+
+@contextmanager
+def get_sender(connection_string, queue_name):
+    with ServiceBusClient.from_connection_string(connection_string) as client, client.get_queue_sender(
+            queue_name) as sender:
+        def send_message(m):
+            message = ServiceBusMessage(body=json.dumps(m, default=lambda o: o.__dict__))
+            logger.info(f'Sending message {message}')
+            start = timeit.default_timer()
+            sender.send_messages(message)
+            logger.info(f'Sent message {message} in {timeit.default_timer() - start}s')
+
+        yield send_message
